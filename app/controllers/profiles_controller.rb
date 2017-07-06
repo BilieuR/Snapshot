6b38e58613eecbab64a8dd06f87a9a3a1f573b80
BilieuR <bilieureath1@gmail>
2017-07-06 18:03:25 +1000

    Finish initial app

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,45 @@
+class ProfilesController < ApplicationController
+  before_action :find_user
+  before_action :authenticate_user!
+  before_action :owned_profile, only: [:edit, :update]
+
+
+  def show
+    @posts = User.find_by(user_name: params[:user_name]).
+    posts.order(created_at: :desc)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(profile_params)
+      flash[:success] = 'Your profile has been updated.'
+      redirect_to profile_path(@user.user_name)
+    else
+      @user.errors.full_messages
+      flash[:error] = @user.errors.full_messages
+      render :edit
+    end
+  end
+
+
+  private
+
+    def find_user
+      @user = User.find_by(user_name: params[:user_name])
+    end
+
+    def owned_profile
+      @user = User.find_by(user_name: params[:user_name])
+
+      unless current_user == @user
+        flash[:alert] = "That profile doesn't belong to you!"
+        redirect_to root_path
+      end
+    end
+
+    def profile_params
+      params.require(:user).permit(:avatar, :bio)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  # get 'profiles/show'
+
   root 'posts#index'
 
   resources :posts do
@@ -7,4 +9,10 @@ Rails.application.routes.draw do
   end
 
   devise_for :users, controllers: { registrations: 'registrations' }
+
+  get ':user_name', to: 'profiles#show', as: :profile
+
+  get ':user_name/edit', to: 'profiles#edit', as: :edit_profile
+
+  patch ':user_name/edit', to: 'profiles#update', as: :update_profile
 end
